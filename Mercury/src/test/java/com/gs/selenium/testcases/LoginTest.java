d5475ddfaf094f26edf4c3080f391d0d36c6b0d8
Sujyont <Sujyont@gmail.com>
2019-02-10 12:10:39 +0530

    First Git Commit.

---
+++ Mercury/src/test/java/com/gs/selenium/testcases/LoginTest.java
@@ -0,0 +1,46 @@
+package com.gs.selenium.testcases;
+
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import com.aventstack.extentreports.Status;
+
+import baseutil.TestBase;
+import pagefunction.LoginPage;
+import utility.ExcelReader;
+
+public class LoginTest extends TestBase{
+	public static final Logger LOG = LogManager.getLogger(LoginTest.class);
+	ExcelReader excelReader = new ExcelReader(testDataPath, "LoginData");
+	
+	@DataProvider(name = "data-provider")
+	 public Object[][] dataProviderMethod() throws Exception {
+		Object[][] data = excelReader.getDataFromSpreadSheet();
+		return data;
+	 }
+	/**
+	 * This method verify Login Test
+	 */
+	@Test(dataProvider = "data-provider")
+	public void login(String userName,String password) {
+		test = extent.createTest("Login Test");
+		launchBrowser();
+		
+		new LoginPage().login(userName,password);
+		
+		//Assert.assertEquals(6,7);
+	
+	}
+	
+	@AfterTest
+	public void tearDown() {
+		driver.close();
+		LOG.info("Browser closed");
+		test.log(Status.INFO, "Browser closed");
+		extent.flush();
+	}
+}
